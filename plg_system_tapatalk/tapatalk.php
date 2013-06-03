67bf84ac324093a97a7d91453b78612ae612ebc2
TomWu <phperwuhan@gmail.com>
2013-06-03 10:23:27 +0800

    improve smartbanner
    Signed-off-by: TomWu <phperwuhan@gmail.com>

--- plg_system_tapatalk/tapatalk.php
+++ plg_system_tapatalk/tapatalk.php
@@ -57,7 +57,8 @@ class plgSystemTapatalk extends JPlugin
     	}
     	*/
     	
-    	//smartbanner
+    	//old smartbanner code
+    	/*
         $pathSmartbanner = 'mobiquo/custom/customSmartbanner.php';
         if (JRequest::getCmd('option') == 'com_kunena' && is_file($pathSmartbanner)) {
             require_once($pathSmartbanner);
@@ -88,6 +89,47 @@ class plgSystemTapatalk extends JPlugin
             $str .= 'var app_location_url = "'.MbqSmartbanner::$MBQ_SMARTBANNER_APP_LOCATION_URL.'";';
             $str .= '</script><script src="'.$tapatalk_dir_url.'/smartbanner/appbanner.js" type="text/javascript"></script>';
             $str .= '<!-- Tapatalk Banner head end-->';
+            $str .= '</head>';
+            $buffer = preg_replace('/<\/head>/i', $str, $buffer, 1);
+    		//body code
+            $str = '<!-- Tapatalk Banner body start -->';
+            $str .= '<script type="text/javascript">tapatalkDetect();</script>';
+            $str .= '<!-- Tapatalk Banner body end -->';
+            $buffer = preg_replace('/<body([^>]*?)>/i', '<body$1>'.$str, $buffer, 1);
+    
+    		JResponse::setBody($buffer);
+    		return true;
+    	} else {
+    	    return false;
+    	}
+    	*/
+    	
+    	//smartbanner
+        $pathSmartbanner = 'mobiquo/custom/customSmartbanner.php';
+        if (JRequest::getCmd('option') == 'com_kunena' && is_file($pathSmartbanner)) {
+            require_once($pathSmartbanner);
+    		$base	= JURI::base(false).'';
+    		$buffer = JResponse::getBody();
+    		
+    		if (MbqSmartbanner::$IS_MOBILE_SKIN) $is_mobile_skin = MbqSmartbanner::$IS_MOBILE_SKIN;
+    		if (MbqSmartbanner::$APP_IOS_ID) $app_ios_id = MbqSmartbanner::$APP_IOS_ID;
+    		if (MbqSmartbanner::$APP_ANDROID_ID) $app_android_id = MbqSmartbanner::$APP_ANDROID_ID;
+    		if (MbqSmartbanner::$APP_KINDLE_URL) $app_kindle_url = MbqSmartbanner::$APP_KINDLE_URL;
+    		if (MbqSmartbanner::$APP_BANNER_MESSAGE) $app_banner_message = MbqSmartbanner::$APP_BANNER_MESSAGE;
+    		$oKunenaConfig = KunenaFactory::getConfig();
+    		MbqSmartbanner::$APP_FORUM_NAME = $oKunenaConfig->board_title;
+    		$app_forum_name = MbqSmartbanner::$APP_FORUM_NAME;
+            MbqSmartbanner::$APP_LOCATION_URL = 'tapatalk://'.preg_replace('/http[s]?\:\/\/(.*?)/i', '$1', $base).'?location=index';
+            $app_location_url = MbqSmartbanner::$APP_LOCATION_URL;
+            MbqSmartbanner::$BOARD_URL = substr($base, 0, strlen($base) - 1);       //!
+            $board_url = MbqSmartbanner::$BOARD_URL;
+            $tapatalk_dir = MbqSmartbanner::$TAPATALKDIR;
+            MbqSmartbanner::$TAPATALKDIR_URL = $base.MbqSmartbanner::$TAPATALKDIR;
+            $tapatalk_dir_url = MbqSmartbanner::$TAPATALKDIR_URL;
+            if (file_exists($tapatalk_dir . '/smartbanner/head.inc.php'))
+                require_once($tapatalk_dir . '/smartbanner/head.inc.php');
+            //header code
+            $str = $app_head_include;
             $str .= '</head>';
             $buffer = preg_replace('/<\/head>/i', $str, $buffer, 1);
     		//body code
