4ac3b40c840155b20555b9be9edb438d370c8459
knigherrant <knigherrant@gmail.com>
2014-09-18 11:56:32 +0700

    Fix class KunenaActivity not found

--- plg_system_tapatalk/activity.php
+++ plg_system_tapatalk/activity.php
@@ -10,51 +10,52 @@ jimport('joomla.plugin.plugin');
 /**
  * integrated with Kunena
  */
-class KunenaActivityTapatalk extends KunenaActivity
-{
-    
-    /**
-     * for thank
-     *
-     * @param  Integer  $target  user id who start the thank action
-     * @param  Integer  $actor  message owner
-     * @param  Object  $message  KunenaForumMessage object
-     */
-    public function onAfterThankyou($target, $actor, $message) {
-        $pushPath = 'mobiquo/push/TapatalkPush.php';
-        require_once($pushPath);
-        $oTapatalkPush = new TapatalkPush();
-        $oTapatalkPush->callMethod('doPushThank', array(
-            'oKunenaForumMessage' => $message
-        ));
-    }
-    
-    /**
-     * for new topic
-     */
-	public function onAfterPost($message) {
-        $pushPath = 'mobiquo/push/TapatalkPush.php';
-        require_once($pushPath);
-        $oTapatalkPush = new TapatalkPush();
-        $oTapatalkPush->callMethod('doPushNewtopic', array(
-            'oKunenaForumMessage' => $message
-        ));
+ if(!class_exists('KunenaActivity')){
+	class KunenaActivityTapatalk extends KunenaActivity
+	{
+		
+		/**
+		 * for thank
+		 *
+		 * @param  Integer  $target  user id who start the thank action
+		 * @param  Integer  $actor  message owner
+		 * @param  Object  $message  KunenaForumMessage object
+		 */
+		public function onAfterThankyou($target, $actor, $message) {
+			$pushPath = 'mobiquo/push/TapatalkPush.php';
+			require_once($pushPath);
+			$oTapatalkPush = new TapatalkPush();
+			$oTapatalkPush->callMethod('doPushThank', array(
+				'oKunenaForumMessage' => $message
+			));
+		}
+		
+		/**
+		 * for new topic
+		 */
+		public function onAfterPost($message) {
+			$pushPath = 'mobiquo/push/TapatalkPush.php';
+			require_once($pushPath);
+			$oTapatalkPush = new TapatalkPush();
+			$oTapatalkPush->callMethod('doPushNewtopic', array(
+				'oKunenaForumMessage' => $message
+			));
+		}
+		
+		/**
+		 * for reply post
+		 */
+		public function onAfterReply($message) {
+			$pushPath = 'mobiquo/push/TapatalkPush.php';
+			require_once($pushPath);
+			$oTapatalkPush = new TapatalkPush();
+			$oTapatalkPush->callMethod('doPushReply', array(
+				'oKunenaForumMessage' => $message
+			));
+		}
+		
 	}
-    
-    /**
-     * for reply post
-     */
-	public function onAfterReply($message) {
-        $pushPath = 'mobiquo/push/TapatalkPush.php';
-        require_once($pushPath);
-        $oTapatalkPush = new TapatalkPush();
-        $oTapatalkPush->callMethod('doPushReply', array(
-            'oKunenaForumMessage' => $message
-        ));
-	}
-	
 }
-
 
 if(JPluginHelper::isEnabled('system', 'uddeim_hooks')){
     JPluginHelper::importPlugin('system','uddeim_hooks');
